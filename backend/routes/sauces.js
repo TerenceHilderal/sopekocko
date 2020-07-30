46c7f5da4efdb5b3439df7b73e768483c61413ab
Hilderal Térence <terencehilderal@gmail.com>
2020-07-30 23:57:04 +0200

    creation du routeur pour les sauces

---
+++ backend/routes/sauces.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/', sauceCtrl.getAllSauce)
+router.get('/:id', sauceCtrl.getOneSauce)
+router.post('/', sauceCtrl.createSauce)
+router.put('/:id', sauceCtrl.modifySauce)
+router.delete('/:id', sauceCtrl.deleteSauce)
+router.post('/:id/like', sauceCtrl.likeSauce)
+
+
+module.exports = router
