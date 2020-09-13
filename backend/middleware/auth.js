dbea594b15c19ec081a1dab855fb4eb5e09d250a
Hilderal Térence <terencehilderal@gmail.com>
2020-09-14 00:44:35 +0200

    its working

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
+const models = require('../models')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
+    const user = await models.User.findOne({ where: { user_id: decodedToken.user_id } })
+    if (!user) {
       throw 'Invalid user ID';
     } else {
+      req.user = user
       next();
     }
   } catch {
